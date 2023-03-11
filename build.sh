b52bf108018ec88212b5a6f6b275b4f9253012ae
Jhon Mario Moreno Ruiz <Jhon.Moreno@recoveryserv.local>
2023-03-11 17:33:15 -0500

    pip install upgrade11

--- build.sh
+++ build.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 pip install --upgrade pip
+pip install -r requirements.txt
 # exit on error
 set -o errexit
 
 
 
 # poetry install
-# pip install -r requirements.txt
+
 # pip install --upgrade pip
 
 
