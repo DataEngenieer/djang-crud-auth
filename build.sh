8259b08276fd24ec7c930962b0793761ec9048de
Jhon Mario Moreno Ruiz <Jhon.Moreno@recoveryserv.local>
2023-03-11 17:08:43 -0500

    pip install upgrade7

--- build.sh
+++ build.sh
@@ -1,12 +1,10 @@
-python -m pip install --upgrade pip
-./deploy-script.bat
 #!/usr/bin/env bash
 # exit on error
 set -o errexit
 
 # poetry install
 # pip install -r requirements.txt
-#pip install --upgrade pip
+pip install --upgrade pip
 
 
 python manage.py collectstatic --no-input
